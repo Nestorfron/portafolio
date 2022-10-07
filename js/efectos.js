2b56419f10ff9daec55f01bea4427a024b97db11
Nestorfron <115091611+Nestorfron@users.noreply.github.com>
2022-10-07 09:51:19 -0300

    animación scroll funcionando

--- js/efectos.js
+++ js/efectos.js
@@ -1,36 +1,11 @@
-$(document).ready(function() {
-	
-        var Inicio = $('#inicio').offset().top,
-            Conocimientos = $('#conocimientos').offset().top,
-            Proyectos = $('#proyectos').offset().top,
-            Contacto = $('#contacto').offset().top;
-    
-    
-        $('#btn-inicio').on('click', function(e){
-            e.preventDefault();
-            $('html, body').animate ({
-                scrollTop: Inicio
-            }, 500);
-        });
-    
-        $('#btn-conocimientos').on('click', function(e){
-            e.preventDefault();
-            $('html, body').animate ({
-                scrollTop: Conocimientos
-            }, 500);
-        });
-    
-        $('#btn-proyectos').on('click', function(e){
-            e.preventDefault();
-            $('html, body').animate ({
-                scrollTop: Proyectos
-            }, 500);
-        });
-    
-        $('#btn-contacto').on('click', function(e){
-            e.preventDefault();
-            $('html, body').animate ({
-                scrollTop: Contacto
-            }, 500);
-        });
-    });
+$(document).ready(function(){
+   var Conocimientos = $('#conocimientos').offset().top;
+   $('#btn-conocimientos').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 380
+        }, 500)
+    }); 
+
+
+
+});
