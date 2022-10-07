7f1fa62d3aa0192ff6a651c725ec279712888d0e
Nestorfron <115091611+Nestorfron@users.noreply.github.com>
2022-10-07 09:51:41 -0300

    scroll arreglado

--- js/efectos.js
+++ js/efectos.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-   var Conocimientos = $('#conocimientos').offset().top;
-   $('#btn-conocimientos').on('click', function(){
+    
+    var concimientos = $('#conocimientos').offset().top;
+    var proyectos = $('#proyectos').offset().top;
+    var contacto = $('#contacto').offset().top;
+
+    $('#btn-conocimientos').on('click', function(){
         $('html, body').animate({
-            scrollTop: 380
-        }, 500)
-    }); 
+            scrollTop: concimientos
+        }, 500);
+    })
 
-
-
+    $('#btn-proyectos').on('click', function(){
+        $('html, body').animate({
+            scrollTop: proyectos
+        }, 500);
+    })
+    
+    $('#btn-contacto').on('click', function(){
+        $('html, body').animate({
+            scrollTop: contacto -50
+        }, 500);
+    })
 });
